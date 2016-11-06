4687a1ac77c46d8e810a3c634696ae3962f1df4f
Sergio Gordillo <sgordillogallardo@gmail.com>
2016-11-06 19:44:26 +0100

    contact auto register

--- src/mail/controllers/send.py
+++ src/mail/controllers/send.py
@@ -15,42 +15,68 @@ def execute(payload):
         recipient = payload['recipient']
         subject = payload['subject']
         content = payload['content']
+        template = None
+        store = True # change? yep
+
         if not content:
             content = 'notset'
         if not subject:
             subject = 'notset'
-        template = None
         if 'template' in payload:
             template = payload['template'] # template.id & template.data
+        if 'store' in payload:
+            store = payload['store']
     except Exception as e:
         return {'status':400, 'error_message':"Wrong payload: "+str(e)}
 
-    return SendMail(recipient, subject, content, template).execute() # send
+    return SendMail(recipient, subject, content, template, store).execute() # send
 
 class SendMail:
-    def __init__(self, recipient, subject, content, template):
-        self.recipient = Email(recipient)
+    def __init__(self, recipient, subject, content, template, store):
+        self.recipient = recipient
         self.subject = subject
         self.content = Content('text/html', content)
         self.template = template
-        self.sender = Email(SENDGRID_API_SENDER)
+        self.sender = SENDGRID_API_SENDER
+        self.store = store
+        self.sg = sendgrid.SendGridAPIClient(apikey=SENDGRID_API_KEY)
+
 
     def execute(self):
-        sg = sendgrid.SendGridAPIClient(apikey=SENDGRID_API_KEY)
-        mail = Mail(self.sender, self.subject, self.recipient, self.content)
+        mail = Mail(Email(self.sender), self.subject, Email(self.recipient), self.content)
 
         if self.template:
             self.set_template(mail)
 
-        response = sg.client.mail.send.post(request_body=mail.get())
+        response = self.sg.client.mail.send.post(request_body=mail.get())
 
         if (response.status_code != 200) and (response.status_code != 202):
             return {'status':response.status_code, 'error_message':response.body}
 
-        return {'message': 'Email Sent'}
+        if self.store:
+            return self.store_contact()
+        else:
+            return {'message': 'Email Sent'}
 
     def set_template(self, mail):
         if self.template['id'] == "c348a464-4240-4cf4-9c88-7b2c892070d7":
             name = self.template['data']['name'].encode('utf-8')
             mail.personalizations[0].add_substitution(Substitution("-name-", name))
             mail.set_template_id(self.template['id'])
+
+    def store_contact(self):
+        contact_data = [
+            {
+                "email": self.recipient,
+                "first_name": "" #@TODO last name?
+            }
+        ]
+
+        if self.template != None and 'name' in self.template['data']:
+            contact_data[0]['first_name'] = self.template['data']['name']
+
+        response = self.sg.client.contactdb.recipients.post(request_body=contact_data)
+        if (response.status_code != 200) and (response.status_code != 201):
+            return {'status':response.status_code, 'error_message':response.body}
+
+        return {'message': 'Email Sent and Contact Saved'}
--- src/mail/main.py
+++ src/mail/main.py
@@ -4,5 +4,5 @@ from flask import Flask
 from src.helpers.generic_errors.main import app
 from routes.api.v1.endpoints import endpoints_routes
 
-#app = Flask(__name__) # Flask app -> From helpers generic app
+# generic app from error_main
 app.register_blueprint(endpoints_routes) # Matching routing (api/v1)
